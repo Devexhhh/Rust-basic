c8daabe4c0e0b4bf9674583bf5b146bdcba02edb
Devexhhh <kumarsujal.codes@gmail.com>
2026-05-13 19:58:16 +0530

    added string basics

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,17 @@
 fn main() {
+    let name: String = String::from("Devex"); 
     let ans = sum(1,2);
+    let even:bool = is_even(10);
+
     println!("{}", ans);
+    println!("{}", even);
+    println!("First name - {}", name);
 }
 
 fn sum(a: u32,b: u32) -> u32 {
     return a+b;
+}
+
+fn is_even(a: u32) -> bool {
+    return a%2 == 0; 
 }
