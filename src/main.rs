b6681c7965830526df2b055aa8afc83fdefdb87d
Devexhhh <kumarsujal.codes@gmail.com>
2026-05-13 21:07:00 +0530

    added borrowing logic code

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,13 @@
 fn main() {
-    let str = String::from("Devex");
-    let len = get_length(str);
+    let mut s1 = String::from("Devex");
+    let s2 = &mut s1;
+    
+    s2.push_str(" Daddy");
 
-    println!("{}", len);
+    let s3 = &s1;
+    let s4 = &s1;
+
+    println!("{} {}", s3, s4);
     
     // get_length(str); // moved the value, so OWNERSHIP is with get_length
 }
