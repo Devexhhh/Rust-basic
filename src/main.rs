7f51301c12db6eb959a6814e0b5c1f40dd5d1a0f
Devexhhh <kumarsujal.codes@gmail.com>
2026-05-13 20:44:19 +0530

    added ownership learning code

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,12 @@
 fn main() {
-    let name: String = String::from("Devex"); 
-    let ans = sum(1,2);
-    let even:bool = is_even(10);
-    let v: Vec<i32> = vec![1,2,3];
+    let str = String::from("Devex");
+    let len = get_length(str);
 
-
-    let mut names:String = String::from("Devex");
-    names.push_str(" Daddy");
-
-    println!("{}", names);
-
-    println!("{}", ans);
-    println!("{}", even);
-    println!("First name - {}", name);
-    println!("{:?}", v);
-    looper();
+    println!("{}", len);
+    
+    // get_length(str); // moved the value, so OWNERSHIP is with get_length
 }
 
-fn sum(a: u32,b: u32) -> u32 {
-    return a+b;
-}
-
-fn is_even(a: u32) -> bool {
-    return a%2 == 0; 
-}
-
-fn looper() {
-    for i in 1..3 {
-        println!("{}", i);
-    }
+fn get_length(str: String) -> usize {
+    return str.len();
 }
