4c36f0839b5fd6840d7fa46f61f4824481559b68
Devexhhh <kumarsujal.codes@gmail.com>
2026-05-13 21:16:45 +0530

    learned struct and impl

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+struct  Rect {
+    height: f32,
+    width: f32
+}
+
+impl Rect {
+    fn area(&self) -> f32 {
+        return self.height * self.width;
+    }
+
+    fn print_something() {
+        println!("Static function")
+    }
+}
+
 fn main() {
     let mut s1 = String::from("Devex");
     let s2 = &mut s1;
@@ -7,8 +22,17 @@ fn main() {
     let s3 = &s1;
     let s4 = &s1;
 
-    println!("{} {}", s3, s4);
+    println!("{}, {}", s3, s4);
     
+
+    let r = Rect {
+        width: 10.0,
+        height: 10.0,
+    };
+
+    println!("{}, {}", r.height, r.width);
+    println!("{}", r.area());
+    Rect::print_something();
     // get_length(str); // moved the value, so OWNERSHIP is with get_length
 }
 
