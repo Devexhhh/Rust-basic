7c511aea16db2b466e2e7fba56160569e1ad8ad4
Devexhhh <kumarsujal.codes@gmail.com>
2026-06-16 22:32:28 +0530

    add dotenv implementation

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,20 @@
-// use chrono::prelude::*;
-use chrono::{Utc, Local};
+use chrono::prelude::*;
+use dotenv::dotenv;
+use std::env;
+// use chrono::{Utc, Local};
 
 fn main() {
+    dotenv().ok();
+
     let utc = Utc::now();
     let local_time = Local::now();
     print!("{}", utc);
     print!("{}", local_time);
+
+    let var = env::var("DATABASE_URL");
+    match var {
+        Ok(str) => println!("\n{}", str),
+        Err(_e) => println!("\nError while reading variable")
+    }
+    // println!("{}", var);
 }
