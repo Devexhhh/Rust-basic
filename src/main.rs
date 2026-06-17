2b877785a2521c0031fee5e65289ba62fda1effc
Devexhhh <kumarsujal.codes@gmail.com>
2026-06-17 13:00:17 +0530

    declarative and procedural macros

--- src/main.rs
+++ src/main.rs
@@ -1,45 +1,28 @@
-use chrono::prelude::*;
-use dotenv::dotenv;
-use std::env;
-// use chrono::{Utc, Local};
-
-#[derive(Copy, Clone)]
-struct Rect<T> {
-    width: T,
-    height: T
-}
-
-impl<T: std::ops::Mul<Output = T> + Copy> Rect<T> {
-    fn area(&self) -> T {
-        return self.width * self.height
-    }
-}
-
-trait Shape {
-    fn area(&self) -> u32;
-}
-
-fn get_area<T: Shape>(s: T) -> u32 {
-    return s.area();
-}
-fn main() {
-    dotenv().ok();
-
-    let utc = Utc::now();
-    let local_time = Local::now();
-    print!("{}", utc);
-    print!("{}", local_time);
-
-    let var = env::var("DATABASE_URL");
-    match var {
-        Ok(str) => println!("\n{}", str),
-        Err(_e) => println!("\nError while reading variable")
-    }
-
-    let r = Rect{
-        width: 10,
-        height: 10
+macro_rules! say_hello {
+    () => {
+        //declarative macros
+        println!("Hello, World!");
     };
-    println!("{}", r.area());
-    // println!("{}", var);
+}
+
+#[derive(Debug)]
+struct User {
+    username: String,
+    password: String,
+    age: u32
+}
+
+fn main() {
+    println!("Hello World");
+    say_hello!();
+
+    let u = User {
+        username: String::from("Devex"),
+        password: String::from("Devex"),
+        age: 22
+    };
+    print!("{:?}", u); // debug
+
+    let v = vec![1,2,3];
+    print!("{:?}", v);
 }
