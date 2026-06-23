5b9be864654625a0001ee183b6bd73b25e07175a
Devexhhh <kumarsujal.codes@gmail.com>
2026-06-23 22:16:47 +0530

    modified main file

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,21 @@
-macro_rules! say_hello {
-    () => {
-        //declarative macros
-        println!("Hello, World!");
-    };
-}
+use serde::{Serialize, Deserialize};
 
-#[derive(Debug)]
+#[derive(Serialize, Deserialize)]
+
 struct User {
     username: String,
-    password: String,
-    age: u32
+    password: String
 }
 
 fn main() {
-    println!("Hello World");
-    say_hello!();
-
     let u = User {
         username: String::from("Devex"),
-        password: String::from("Devex"),
-        age: 22
+        password: String::from("123241")
     };
-    print!("{:?}", u); // debug
 
-    let v = vec![1,2,3];
-    print!("{:?}", v);
+    let serialized_string = serde_json::to_string(&u);
+    match serialized_string {
+        Ok(str) => print!("{}\n", str),
+        Err(_) => print!("Error while converting to string")
+    }
 }
